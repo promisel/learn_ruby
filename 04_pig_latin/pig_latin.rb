34e5e766c9593d451644341575e0b70d14cfd57f
promisel <prochin2001@gmail.com>
2020-01-23 00:49:50 +0800

    Finish exercise 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,21 @@
 #write your code here
+
+VOWELS = "aeiou"
+
+def pig_word word
+  head_index = word.split("").index {|ch| VOWELS.include?(ch) }
+
+  if head_index == 0
+    return word + "ay"
+  end
+
+  if word[head_index] == "u" and word[head_index - 1] == "q"
+    head_index += 1
+  end
+
+  word[head_index..-1] + word[0..head_index - 1] + "ay"
+end
+
+def translate str
+  str.split().map {|word| pig_word word }.join(" ")
+end
