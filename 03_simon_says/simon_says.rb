c8e30e07cf8f92c0816e74920bf4696d64452591
promisel <prochin2001@gmail.com>
2020-01-22 21:34:22 +0800

    Finish exercise 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
 #write your code here
+
+def echo str
+  str
+end
+
+def shout str
+  str.upcase
+end
+
+def repeat(str, times=2)
+  ([str] * times).join(" ")
+end
+
+def start_of_word(word, num)
+  word[0..(num - 1)]
+end
+
+def first_word str
+  str.split()[0]
+end
+
+LITTLE_WORDS = ["and", "over", "the"]
+
+def cap(word)
+  unless LITTLE_WORDS.include?(word)
+    return word.capitalize
+  end
+  word
+end
+
+def titleize str
+  str.capitalize.split().map {|word| cap(word) }.join(" ")
+end
