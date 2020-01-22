9d6de0292f2233489ad40038360e30b26fe9bc5e
promisel <prochin2001@gmail.com>
2020-01-22 19:36:47 +0800

    Finish exercise 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,29 @@
 #write your code here
+
+def add(num, other)
+  num + other
+end
+
+def subtract(num, other)
+  num - other
+end
+
+def sum array
+  total = 0
+  array.each do |item|
+    total += item
+  end
+  total
+end
+
+def multiply(*nums)
+  nums.reduce(:*)
+end
+
+def power(base, ex)
+  base ** ex
+end
+
+def factorial num
+  (1..num).reduce(1, :*)
+end
