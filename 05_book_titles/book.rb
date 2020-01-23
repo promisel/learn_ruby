d6e8b44fbc9302d59ddd49874d5fc0c34d6be3f4
promisel <prochin2001@gmail.com>
2020-01-23 11:23:29 +0800

    Finish exercise 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,26 @@
 class Book
-# write your code here
+  # write your code here
+
+  def title
+    @title
+  end
+
+  def title=(title)
+    @title = titleize(title)
+  end
+
+  private
+
+  LITTLE_WORDS = ["and", "over", "the", "a", "an", "in", "of"]
+
+  def cap(word)
+    unless LITTLE_WORDS.include?(word)
+      return word.capitalize
+    end
+    word
+  end
+
+  def titleize str
+    str.capitalize.split().map {|word| cap(word) }.join(" ")
+  end
 end
