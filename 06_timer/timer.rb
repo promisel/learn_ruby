982b46c7c0d52bf84c47d9f03f03bd8058246b99
promisel <prochin2001@gmail.com>
2020-01-23 11:52:10 +0800

    Finish exercise 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,21 @@
 class Timer
   #write your code here
+
+  ZERO = Time.new(0)
+
+  def initialize
+    @time = ZERO
+  end
+
+  def seconds
+    @time.nsec
+  end
+
+  def seconds=(seconds)
+    @time = ZERO + seconds
+  end
+
+  def time_string
+    @time.strftime("%T")
+  end
 end
